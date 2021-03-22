58889608aabe65cd80162ecab52e5f49a07f8042
Aryan Chavan <aryanchavan2003@gmail.com>
2021-03-22 17:51:38 +0000

    finish ledc.py

--- ledc.py
+++ ledc.py
@@ -1,11 +1,18 @@
 from gpiozero import RGBLED
+import time
+
+delay = 0.1
 
 led = RGBLED(red=18, green=15, blue=14)
 
 colors = {
+            "white": (0, 0, 0),
             "red": (0, 1, 1),
             "green": (1, 0, 1),
-            "blue": (1, 1, 0)
+            "blue": (1, 1, 0),
+            "yellow": (0, 0, 1),
+            "cyan": (1, 0, 0),
+            "magenta": (0, 1, 0)
         }
 
 
@@ -15,6 +22,7 @@ def changeColor(color):
 
 # checking for update
 while True:
-    changeColor(input(">> "))
-
+    time.sleep(delay)
+    with open("color", "r") as f:
+        changeColor(f.read()[:-1])
 
