1345fe9f07d826c5980ae5343dca2d283d9d4dcf
Aryan Chavan <aryanchavan2003@gmail.com>
2021-03-23 10:19:00 +0530

    frontend send color to backend

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ const selectors = document.querySelectorAll(".color-selector");
 
 for (let i = 0; i < selectors.length; i++) {
 	selectors[i].addEventListener("click", (e) => {
-		console.log(e.target.innerText);
+        document.getElementById("selected-color").value = e.target.innerText;
+        document.getElementById("change-color-form").submit();
 	});
 }
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`server live on port ${port}`);
+});
+
+app.post("/change-color", (req, res) => {
+	console.log(req.body.color);
+	res.redirect("/");
 });
