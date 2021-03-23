0dae16fd557130d9d7cb8cf397731b41db22dd87
Aryan Chavan <aryanchavan2003@gmail.com>
2021-03-23 10:30:46 +0530

    write color to f=color file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
+fs = require("fs");
+
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,5 +15,10 @@ app.listen(port, () => {
 
 app.post("/change-color", (req, res) => {
 	console.log(req.body.color);
+	writeToColor(req.body.color);
 	res.redirect("/");
 });
+
+function writeToColor(color) {
+	fs.writeFile(__dirname + "/color", color + "\n", (err, fd) => {});
+}
